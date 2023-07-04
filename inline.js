5d75def8b813da56fdd313287f09e5f3ad5d314a
Ciaran <>
2023-07-04 08:42:16 +0100

    Modify Content-Type header or add if missing.
    Sometimes request return a strange content-type for a PDF such as application/octet-stream.
    
    I am attempting to force/coerce the header into application/pdf to help hint to the browser
    that it should open it using the built-in PDF viewer.

--- inline.js
+++ inline.js
@@ -1,17 +1,27 @@
 "use strict";
 
 function ChangeContentDispositionHeaderToInline(e) {
-    let found = false;
+    let found_content_disposition_header = false;
+    let found_content_type_header = false;
     for (const header of e.responseHeaders) {
         if (header.name.toLowerCase() === 'content-disposition') {
-            found = true;
+            found_content_disposition_header = true;
             header.value = 'inline';
         }
+        if (header.name.toLowerCase() === 'content-type') {
+            found_content_type_header = true;
+            header.value = 'application/pdf';
+        }
     }
-    if (!found) {
-        console.debug(e.responseHeaders, "Did not find header. Adding inline header.");
+    if (!found_content_disposition_header) {
+        console.debug("Did not find Content-Disposition header. Adding the header.");
         e.responseHeaders.push({"name":"content-disposition", "value": "inline"});
     }
+    if (!found_content_type_header){
+        console.debug("Did not find Content-Type header. Adding the header.");
+        e.responseHeaders.push({"name":"content-type", "value": "application/pdf"});
+    }
+    console.debug(e.responseHeaders);
     return {responseHeaders: e.responseHeaders};
 }
 
